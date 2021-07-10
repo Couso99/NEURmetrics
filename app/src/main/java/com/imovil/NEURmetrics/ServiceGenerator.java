6daf0bac065028076915868be6690abd5b71011f
Pablo Couso <42821545+Couso99@users.noreply.github.com>
2021-07-10 20:17:55 +0200

    Update ServiceGenerator.java

--- app/src/main/java/com/imovil/NEURmetrics/ServiceGenerator.java
+++ app/src/main/java/com/imovil/NEURmetrics/ServiceGenerator.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
 
-    private static String BASE_URL = "http://192.168.0.21:5000";//"http://raspberrypi.lan:5000/";
+    private static String BASE_URL = "http://192.168.0.21";//"http://raspberrypi.lan:5000/";
 
     public static boolean setBaseUrl(String baseUrl) {
         if (BASE_URL.equals(baseUrl)) return false;
